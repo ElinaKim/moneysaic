1d25f28fad58a32ad4d45220948e9ed927c13ac1
Elina Kim <elinakim791@gmail.com>
2024-10-15 11:51:40 -0700

    display components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { MainDashboard } from './components/MainDashboard/MainDashboard'
+import { Navbar } from './components/Navbar/Navbar'
+import './index.css'
 
 function App() {
 
   return (
     <>
+      <MainDashboard />
+      <Navbar />
     </>
   )
 }
